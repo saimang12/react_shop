79825f0c1e81cbadf86d3ba1a94616bb3a092606
saimang12 <114594540+saimang12@users.noreply.github.com>
2023-04-24 22:59:37 +0900

    testsxd

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <>asdasdgf22</>
       <App />
     </BrowserRouter>
   </React.StrictMode>
